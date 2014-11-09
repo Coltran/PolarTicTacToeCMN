b749042c3f0a940b0336a3462c72d50453e60cae
Coltran <coltranhn@gmail.com>
2014-11-09 12:42:57 -0700

    started on Game

--- PolarTicTacToeCMN/src/game/Game.java
+++ PolarTicTacToeCMN/src/game/Game.java
@@ -1,5 +1,16 @@
 package game;
-
+//This is the game definition which can be used to keep track of the current game state an implement win checks
 public class Game {
-
+	//board keeps track of game state
+	String[][] board;
+	//winCheck returns the winning player if a player has one, null otherwise
+	public String winCheck() {
+		String winningPlayer = null;
+		return winningPlayer;
+	}
+	//move allows a player to make a move
+	public void move(String player, int x, int y) {
+		
+	}
+	
 }
