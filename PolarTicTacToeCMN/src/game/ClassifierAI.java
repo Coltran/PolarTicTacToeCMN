9fca9908fb9a353a65158569f5aae639f312bf1c
Coltran <coltranhn@gmail.com>
2014-11-29 13:20:02 -0700

    updated heuristicAI to use char

---
+++ PolarTicTacToeCMN/src/game/ClassifierAI.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package game;
+
+/**
+ * @author Coltran
+ *
+ */
+public class ClassifierAI {
+
+	/**
+	 * 
+	 */
+	public ClassifierAI() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
