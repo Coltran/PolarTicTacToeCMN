4c1d4f438bd72f1c2cda2ce3dfcacb3ba708af90
Coltran <coltranhn@gmail.com>
2014-12-07 13:32:02 -0700

    Neural Net AI comments
    Updated NeuralNetAI with the finished code from nearest neighbor and
    added comments describing my understanding of what needs to be done.

--- PolarTicTacToeCMN/src/game/NeuralNetAI.java
+++ PolarTicTacToeCMN/src/game/NeuralNetAI.java
@@ -7,36 +7,33 @@ import java.util.Random;
 
 /**
  * @author Coltran
- * This class Implements the Heuristic based AI to play Polar Tic Tac Toe
- * Instantiate with HeuristicAI name = new HeuristicAI(playerChoice) 
- * where playerChoice is 'X' or 'O'
- * To have the AI make a move: name.move(game) 
- * where game is a Game
+ *
  */
-public class NeuralNetAI implements AI {
+public class NeuralNetAI implements AI{
 	
-	private Character player;//not used currently
+	public Character player;//not used currently
 	private Character opponent;
-	private Board[][] examples;//holds all of our examples to compare against
-	Character result[];//holds the results for all the example games (x,o or d)
 	int numberExamples;//number of example games to run can be chosen by user
-	int similarity[];//holds the similarity to each example at the same turn level
 	private int movex;
 	private int movey;
+	int[] hiddenWeights;
+	int[] outputWeights;
 
 	/**
 	 * 
 	 */
+	//constructor 
 	public NeuralNetAI(Character playerChoice, int inNumberExamples) {
 		player = playerChoice;
-		examples = new Board[inNumberExamples][48];//48 is maximum possible moves in a game
 		numberExamples = inNumberExamples;
-		similarity = new int[inNumberExamples];
-		result = new Character[inNumberExamples];
 		opponent = 'X';
 		if(playerChoice == 'X') {
 			opponent = 'O';
 		}
+		//build neural net construct
+		int[][] hiddenWeights = new int[30][50];
+			//using 30 hidden nodes and 50 input nodes, we have 50 weights for each of the hidden nodes
+		int[] outputWeights = new int[30]; // we have one weight coming into our ourput node for each hidden node
 		learn();
 	}
 	
@@ -51,47 +48,272 @@ public class NeuralNetAI implements AI {
 		else{
 			int ply = 2;//number of plys to search
 			int depth = ply*2;//moves to look ahead = ply * 2
-			//lookAhead(game.board, player, depth);//call recursive lookahead function
+			lookAhead(game.board, player, depth, game);//call recursive lookahead function
 			//lookahead will set movex and movey to best values
 		}
 		boolean win = game.move(player, movex, movey);//make move
-		return win;//return if we wone the game
+		return win;//return if we won the game
 	}
-	
-	
-	//play lots of games, saving every board state and classifying each game as win or loss
-	private void learn() {
-		//loop once for each desired example
-		for(int i=0; i<numberExamples; i++) {
-			Game trainingGame = new Game('x','o');//make a game
-			//Heuristic based AIs to play the game
-			HeuristicAI playerX = new HeuristicAI('x');
-			HeuristicAI playerO = new HeuristicAI('o');
-			int turnNumber = 0;//tracks turn for saving intermediate game boards
-			//until game is done
-			while(trainingGame.done == false) {
-				playerX.move(trainingGame);//first player moves
-				examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
-				turnNumber++;//next turn
-				if(trainingGame.winningPlayer == 'x') {
-					result[i] = 'x';//set result to x win if x wone
-				}
-				//if the game is not done
-				else if(trainingGame.done == false) {
-					playerO.move(trainingGame);//seccond player moves
-					examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
-					turnNumber++;//next turn
-					if(trainingGame.winningPlayer == 'y') {
-						result[i] = 'y';//set result to x win if x won
+	/**
+	 * recursively checks ahead and returns best heuristic value
+	 * sets movex and movey corrosponding to the best move found
+	 * @param board
+	 * @param thisPlayer
+	 * @param depth
+	 * @return
+	 */
+	private Integer lookAhead(Board board, Character thisPlayer, int depth, Game game) {
+		boolean[][] moves = LegalMoves.Moves(board);//all available moves
+		Integer[][] values = new Integer[4][12];//stores heuristic values for each available move
+		int numberLegalMoves = 0;
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<12; j++) {
+				if(moves[i][j]==true) {//if we can move there...
+					numberLegalMoves++;
+					Board candidate = Board.clone(board);//copy the board to pass forward
+					candidate.theBoard[i][j] = thisPlayer;//make move on copy
+					//if we don't need to search any farther
+					if(depth <= 1 || WinCheck.check(i, j, candidate) == true) {
+						if(WinCheck.check(i, j, candidate)==true) {
+							//if we can win
+							if(candidate.theBoard[i][j] == player) {
+								movex = i;
+								movey = j;
+								values[i][j] = 1;
+								return values[i][j];//just return the winning move
+							}
+							//if our opponent won
+							else {
+								values[i][j] = -1;
+							}
+						}
+						//if we need to evaluate the current board
+						else {
+							values[i][j] = evaluate(board);//call heuristic and save returned value
+						}
+					}
+					//if we need to look farther ahead
+					else {
+						//determine next player
+						Character otherPlayer = 'X';
+						if(thisPlayer == 'X') {
+							otherPlayer = 'O';
+						}
+						//recursive function call
+						values[i][j] = lookAhead(candidate, otherPlayer, depth-1, game);
 					}
 				}
-				if(trainingGame.done && trainingGame.winningPlayer == null) {
-					result[i] = 'd';//set result to draw if game is over and nobody won
+				//if we can't move there
+				else {
+					values[i][j] = null;
 				}
 			}
 		}
+		if(numberLegalMoves == 0) {
+			return 0;
+		}
+		//if us (max player)
+		if(thisPlayer == player) {
+			int maxvalue = -999999999;//maximum heuristic value of returned move
+			for(int i=0; i<4; i++) {
+				for(int j=0; j<12; j++) {
+					if(values[i][j] != null && values[i][j] > maxvalue) {
+						maxvalue = values[i][j];
+					}
+				}
+			}
+			//break ties randomly
+			int[] tiesx = new int[48];//will hold x coordinate for ties
+			int[] tiesy = new int[48];//will hold y coordinate for ties
+			int count = 0;//tracks number of ties
+			for(int i=0; i<4; i++) {
+				for(int j=0; j<12; j++) {
+					if(values[i][j] != null && values[i][j] == maxvalue) {//if we found one of the best moves
+						tiesx[count] = i;//add x value to list
+						tiesy[count] = j;//add coorosponding y value to list
+						count++;//increment count
+					}
+				}
+			}
+			Random generator = new Random(System.nanoTime());
+			int randomChoice = generator.nextInt(count);//pick a random best move
+			//make move
+			movex = tiesx[randomChoice];
+			movey = tiesy[randomChoice];
+			return maxvalue;//we will choose best move for us
+		}
+		//otherwise it's our opponent (min player)
+		else {
+			int minvalue = 999999999;//minimum heuristic value of returned move
+			for(int i=0; i<4; i++) {
+				for(int j=0; j<12; j++) {
+					if(values[i][j] != null && values[i][j] < minvalue) {
+						minvalue = values[i][j];
+						movex = i;
+						movey = j;
+					}
+				}
+			}
+			//break ties randomly
+			int[] tiesx = new int[49];//will hold x coordinate for ties
+			int[] tiesy = new int[49];//will hold y coordinate for ties
+			int count = 0;//tracks number of ties
+			for(int i=0; i<4; i++) {
+				for(int j=0; j<12; j++) {
+					if(values[i][j] != null && values[i][j] == minvalue) {//if we found one of the best moves
+						tiesx[count] = i;//add x value to list
+						tiesy[count] = j;//add coorosponding y value to list
+						count++;//increment count
+					}
+				}
+			}
+			Random generator = new Random(System.nanoTime());
+			int randomChoice = generator.nextInt(count);//pick a random best move
+			//make move
+			movex = tiesx[randomChoice];
+			movey = tiesy[randomChoice];
+			return minvalue;//assume opponent will choose the best move for them
+		}
+		
 	}
+	
+	//returns an evaluation of current state using neural net.
+	public int evaluate(Board board) {
+		int[] inputNodes = new int[50];
+		//TODO iniitalize first 48 input nods to values of board locations, last 2 to number moves made by x and by y
+			//for each board space, 0 represents open, 1 represents we have moved there, -1 represents opponent has moved there
+			//number moves made by y should be negative?
+		
+		int[] hiddenNodes = new int[30];
+		//TODO for each hidden node,{ multiply each weight for that node by the value in the corresponding inputNode
+			//sum these 50 results and ?devide by 50? this will give you the value for that hidden node
+		
+		//TODO now repeat this process for the output node, summing over the 30 (output weights * corresponding hiddenNode)
+			//devide by 30? and this will give you the value for the input game state, return this value.
+	}
+	
+	//play lots of games, training neural net on each game. 
+	private void learn() {
+		//TODO initialize hiddenWeights and and outputWeights to random number between -3 and 3 excluding 0
+		//loop once for each desired example
+		for(int i=0; i<numberExamples; i++) {
+			Game trainingGame = new Game('X','O');//make a game
+			//TODO call the move method repeatedly but with alternating players to have the net play the game. 
+				//Before each move, make a copy of all the weights, after each move make a copy of all the node values (including output).
+				//we only need to save these weights and values going back two moves. 
+			//TODO after each move other than the first, update all Weights
+				//to do this, for each node, subtract the most recent value from the previous value (this is the temporal difference part)
+					//for each weight coming into the node, multiply the difference by the inputs contribution and by the learning rate. 
+					//the contribution (i'm not sure about this part as it's the gradient stuff) is something like
+					// the most recent input to that weight times the weight, or maybe how this changed from the previous time
+			//I think that's it. the lookahead, etc shouldn't have to be changed
+		}
+	}
+
 }
+
+
+
+
+
+///**
+// * 
+// */
+//package game;
+//
+//import java.util.Random;
+//
+///**
+// * @author Coltran
+// * This class Implements the Heuristic based AI to play Polar Tic Tac Toe
+// * Instantiate with HeuristicAI name = new HeuristicAI(playerChoice) 
+// * where playerChoice is 'X' or 'O'
+// * To have the AI make a move: name.move(game) 
+// * where game is a Game
+// */
+//public class NeuralNetAI implements AI {
+//	
+//	private Character player;//not used currently
+//	private Character opponent;
+//	private Board[][] examples;//holds all of our examples to compare against
+//	Character result[];//holds the results for all the example games (x,o or d)
+//	int numberExamples;//number of example games to run can be chosen by user
+//	int similarity[];//holds the similarity to each example at the same turn level
+//	private int movex;
+//	private int movey;
+//
+//	/**
+//	 * 
+//	 */
+//	public NeuralNetAI(Character playerChoice, int inNumberExamples) {
+//		player = playerChoice;
+//		examples = new Board[inNumberExamples][48];//48 is maximum possible moves in a game
+//		numberExamples = inNumberExamples;
+//		similarity = new int[inNumberExamples];
+//		result = new Character[inNumberExamples];
+//		opponent = 'X';
+//		if(playerChoice == 'X') {
+//			opponent = 'O';
+//		}
+//		learn();
+//	}
+//	
+//	public boolean move(Game game) {
+//		movex = 0;//best move in x direction (there should always be a valid move when this is called)
+//		movey = 0;//best move in y direction ''
+//		if(game.moveNumber == 0) {//just pick a random move if we're going first
+//			Random generator = new Random(System.nanoTime());
+//			movex = generator.nextInt(4);
+//			movey = generator.nextInt(12);
+//		}
+//		else{
+//			int ply = 2;//number of plys to search
+//			int depth = ply*2;//moves to look ahead = ply * 2
+//			//lookAhead(game.board, player, depth);//call recursive lookahead function
+//			//lookahead will set movex and movey to best values
+//		}
+//		boolean win = game.move(player, movex, movey);//make move
+//		return win;//return if we wone the game
+//	}
+//	
+//	
+//	//play lots of games, saving every board state and classifying each game as win or loss
+//	private void learn() {
+//		//loop once for each desired example
+//		for(int i=0; i<numberExamples; i++) {
+//			Game trainingGame = new Game('x','o');//make a game
+//			//Heuristic based AIs to play the game
+//			HeuristicAI playerX = new HeuristicAI('x');
+//			HeuristicAI playerO = new HeuristicAI('o');
+//			int turnNumber = 0;//tracks turn for saving intermediate game boards
+//			//until game is done
+//			while(trainingGame.done == false) {
+//				playerX.move(trainingGame);//first player moves
+//				examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
+//				turnNumber++;//next turn
+//				if(trainingGame.winningPlayer == 'x') {
+//					result[i] = 'x';//set result to x win if x wone
+//				}
+//				//if the game is not done
+//				else if(trainingGame.done == false) {
+//					playerO.move(trainingGame);//seccond player moves
+//					examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
+//					turnNumber++;//next turn
+//					if(trainingGame.winningPlayer == 'y') {
+//						result[i] = 'y';//set result to x win if x won
+//					}
+//				}
+//				if(trainingGame.done && trainingGame.winningPlayer == null) {
+//					result[i] = 'd';//set result to draw if game is over and nobody won
+//				}
+//			}
+//		}
+//	}
+//}
+
+
+
+
 //last state is last board state
 //the neural net is huge then, every state and every middle state yada yada
 //one hidden layer, but at least 20 nodes in the hidden layer, maybe more
