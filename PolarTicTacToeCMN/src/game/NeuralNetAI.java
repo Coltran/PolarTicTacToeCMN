97b3aaa2fdb8c779ff7bfd6e5ed9fa64cbd9e34a
jedi-wolf <nick.burgard@gmail.com>
2014-12-07 20:22:09 -0700

    Running

--- PolarTicTacToeCMN/src/game/NeuralNetAI.java
+++ PolarTicTacToeCMN/src/game/NeuralNetAI.java
@@ -278,22 +278,18 @@ public class NeuralNetAI implements AI{
 		double[] hiddenNodesPre1 = new double[30], hiddenNodesPre2 = new double[30];
 		double outputNodePre1 = 0, outputNodePre2 = 0;
 		
-		System.out.println("In learn, pre for");
-		
 		for(int i=0; i<numberExamples; i++) {
-			System.out.println("In for");
 			boolean gameDone = false;
 			Game trainingGame = new Game('X','O');//make a game
 			//TODO call the move method repeatedly but with alternating players to have the net play the game. 
 				//Before each move, make a copy of all the weights, after each move make a copy of all the node values (including output).
 				//we only need to save these weights and values going back two moves.
 			
-			System.out.println("made training game");
 			hiddenWeightsPre2 = hiddenWeightsPre1;
 			hiddenWeightsPre1 = hiddenWeights;
 			outputWeightsPre2 = outputWeightsPre1;
 			outputWeightsPre1 = outputWeights;
-			trainingMove(trainingGame, 'X');//Will have to find a way to specify player
+			gameDone = trainingMove(trainingGame, 'X');//Will have to find a way to specify player
 			inputNodesPre2 = inputNodesPre1;
 			inputNodesPre1 = inputNodes;
 			hiddenNodesPre2 = hiddenNodesPre1;
@@ -308,7 +304,7 @@ public class NeuralNetAI implements AI{
 				hiddenWeightsPre1 = hiddenWeights;
 				outputWeightsPre2 = outputWeightsPre1;
 				outputWeightsPre1 = outputWeights;
-				trainingMove(trainingGame, 'O');//Will have to find a way to specify player
+				gameDone = trainingMove(trainingGame, 'O');//Will have to find a way to specify player
 				inputNodesPre2 = inputNodesPre1;
 				inputNodesPre1 = inputNodes;
 				hiddenNodesPre2 = hiddenNodesPre1;
@@ -345,7 +341,7 @@ public class NeuralNetAI implements AI{
 				hiddenWeightsPre1 = hiddenWeights;
 				outputWeightsPre2 = outputWeightsPre1;
 				outputWeightsPre1 = outputWeights;
-				trainingMove(trainingGame, 'X');//Will have to find a way to specify player
+				gameDone = trainingMove(trainingGame, 'X');
 				inputNodesPre2 = inputNodesPre1;
 				inputNodesPre1 = inputNodes;
 				hiddenNodesPre2 = hiddenNodesPre1;
@@ -369,164 +365,6 @@ public class NeuralNetAI implements AI{
 
 }
 
-//if U[currentBoard] does not exist then U[currentBoard] = reward
-//if s (previous state) exists then 
-	//N[s]++
-	//U[s] = U[s] + alpha*(N[s])*(r + gamma*U[s`] - U[s])
-//if s' is end state then
-	//s, a, r = null
-//else
-	//s = currentBaord
-	//a = pi[currenBoard]
-	//r = reward
-
-
-
-///**
-// * 
-// */
-//package game;
-//
-//import java.util.Random;
-//
-///**
-// * @author Coltran
-// * This class Implements the Heuristic based AI to play Polar Tic Tac Toe
-// * Instantiate with HeuristicAI name = new HeuristicAI(playerChoice) 
-// * where playerChoice is 'X' or 'O'
-// * To have the AI make a move: name.move(game) 
-// * where game is a Game
-// */
-//public class NeuralNetAI implements AI {
-//	
-//	private Character player;//not used currently
-//	private Character opponent;
-//	private Board[][] examples;//holds all of our examples to compare against
-//	Character result[];//holds the results for all the example games (x,o or d)
-//	int numberExamples;//number of example games to run can be chosen by user
-//	int similarity[];//holds the similarity to each example at the same turn level
-//	private int movex;
-//	private int movey;
-//
-//	/**
-//	 * 
-//	 */
-//	public NeuralNetAI(Character playerChoice, int inNumberExamples) {
-//		player = playerChoice;
-//		examples = new Board[inNumberExamples][48];//48 is maximum possible moves in a game
-//		numberExamples = inNumberExamples;
-//		similarity = new int[inNumberExamples];
-//		result = new Character[inNumberExamples];
-//		opponent = 'X';
-//		if(playerChoice == 'X') {
-//			opponent = 'O';
-//		}
-//		learn();
-//	}
-//	
-//	public boolean move(Game game) {
-//		movex = 0;//best move in x direction (there should always be a valid move when this is called)
-//		movey = 0;//best move in y direction ''
-//		if(game.moveNumber == 0) {//just pick a random move if we're going first
-//			Random generator = new Random(System.nanoTime());
-//			movex = generator.nextInt(4);
-//			movey = generator.nextInt(12);
-//		}
-//		else{
-//			int ply = 2;//number of plys to search
-//			int depth = ply*2;//moves to look ahead = ply * 2
-//			//lookAhead(game.board, player, depth);//call recursive lookahead function
-//			//lookahead will set movex and movey to best values
-//		}
-//		boolean win = game.move(player, movex, movey);//make move
-//		return win;//return if we wone the game
-//	}
-//	
-//	
-//	//play lots of games, saving every board state and classifying each game as win or loss
-//	private void learn() {
-//		//loop once for each desired example
-//		for(int i=0; i<numberExamples; i++) {
-//			Game trainingGame = new Game('x','o');//make a game
-//			//Heuristic based AIs to play the game
-//			HeuristicAI playerX = new HeuristicAI('x');
-//			HeuristicAI playerO = new HeuristicAI('o');
-//			int turnNumber = 0;//tracks turn for saving intermediate game boards
-//			//until game is done
-//			while(trainingGame.done == false) {
-//				playerX.move(trainingGame);//first player moves
-//				examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
-//				turnNumber++;//next turn
-//				if(trainingGame.winningPlayer == 'x') {
-//					result[i] = 'x';//set result to x win if x wone
-//				}
-//				//if the game is not done
-//				else if(trainingGame.done == false) {
-//					playerO.move(trainingGame);//seccond player moves
-//					examples[i][turnNumber] = Board.clone(trainingGame.board);//save game board
-//					turnNumber++;//next turn
-//					if(trainingGame.winningPlayer == 'y') {
-//						result[i] = 'y';//set result to x win if x won
-//					}
-//				}
-//				if(trainingGame.done && trainingGame.winningPlayer == null) {
-//					result[i] = 'd';//set result to draw if game is over and nobody won
-//				}
-//			}
-//		}
-//	}
-//}
-
-
-
-
-//last state is last board state
-//the neural net is huge then, every state and every middle state yada yada
-//one hidden layer, but at least 20 nodes in the hidden layer, maybe more
-//train once, not every time we run?
-
-//Some websites
-
-//http://en.literateprograms.org/Perceptron_%28Java%29
-//https://code.google.com/p/aima-java/		go to TD code
-//http://aima-java.googlecode.com/svn/trunk/aima-all/release/aima3ejavademos.html		has tictactoe minmax and AlphaBeta
-//http://aispace.org/neural/help/tutorial4.shtml
-
-
-//loop and find all legal moves
-//These moves are the input to the current state in the neural net?
-//Whichever move is best would be weighted 1, rest would be -1.
-	//Maybe have some at 0? But no reason to do that
-
-//Run TD function on any play
-//Only have a reward on win, loss, or draw
-//TD is recursive
-//can enter any state to start with, will go to end.
-	//Opposing playing will return multiple different results to any state, this should be factored in automatically after enough tests
-
-//Input is current state, outputs are all possible legal moves?
-
-
-
-//input percept indicating current state s` and reward r`
-//persistent
-	//pi is fixed policy?
-	//U[] is table of utilities, initially empty (UTILITY IS WEIGHT!!)
-	//N[] is table of frequencies, initially zero
-	//s, a, r are previous state, previous action, and previous reward, initially null
-//if s` is new then U[s`]=r`
-//if s is not null then
-	//N[s]++
-	//U[s] = U[s] + alpha*(N[s])*(r + gamma*U[s`] - U[s])
-//if s`.TERMINAL? then s, a, r = null else s, a, r = s`,pi[s`],r` (end state)
-//return a
-
-//1, 0, -1 are reward options
-//gamma is discount factor set to 1 for now, never more then 1?
-//alpha is learning rate, don't know what to set as
-
-
-//input current board and reward (0?)
 //if U[currentBoard] does not exist then U[currentBoard] = reward
 //if s (previous state) exists then 
 	//N[s]++
